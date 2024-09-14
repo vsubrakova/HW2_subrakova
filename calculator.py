5b1f06d61a48f88353e6246721842c44ff945f5b
viburnum <viburnum@MacBook-Pro-Anna.local>
2024-09-15 00:18:13 +0300

    main function

--- calculator.py
+++ calculator.py
@@ -2,3 +2,13 @@
 # Input - user enters numbers and operation sign
 # Output - calculated number
 # Authors: Калинина, Козлова, Субракова, Финк
+inp = input()
+
+
+def main(inp):
+    string_to_list = inp.split(" ")
+    a = int(string_to_list[0])
+    b = int(string_to_list[2])
+    operator = string_to_list[1]
+    return a, b, operator
+
