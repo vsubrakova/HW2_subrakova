ef7550e2af04e95d78c8f4710a0a0d08bcf2b920
vsubrakova <101574741+vsubrakova@users.noreply.github.com>
2024-09-14 16:45:28 +0200

    Update calculator.py
    Add description

--- calculator.py
+++ calculator.py
@@ -1,1 +1,4 @@
-
+# Description - program calculates basic mathematical operations: +,-,*,/
+# Input - user enters numbers and operation sign
+# Output - calculated number
+# Authors: Калинина, Козлова, Субракова, Финк
