50efc3be787e64863611ba5ead41d880242810d7
vsubrakova <subrakova.vera@gmail.com>
2024-09-15 15:20:20 +0200

    Add multiply function

--- calculator.py
+++ calculator.py
@@ -7,18 +7,28 @@
 inp = input()
 
 def main(inp):
-    string_to_list = inp.split(" ")
+
+    string_to_list = inp.split("введите выражение, используя пробелы до и после оператора, дробные числа должны быть разделены .")
     a = float(string_to_list[0])
     b = float(string_to_list[2])
     operator = string_to_list[1]
     return a, b, operator
 
+
 x, y, operator = main(inp)
 
 def add(x, y):
        return x + y
+def multiply(x,y):
+       return x*y
 
 if operator == '+':
-    print(add(x,y))
+    result = add(x,y)
+elif operator == '*':
+    result = multiply(x*y)
+
+ print(f"Результат: {result}")
+    
+
 
 
