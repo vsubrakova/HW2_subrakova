25298b0bc397d8a293abdc4f8e086f109adb9634
The-AGT <maksim.fink@pharminnotech.com>
2024-09-15 17:36:11 +0300

    Update calculator.py
    Правки

--- calculator.py
+++ calculator.py
@@ -7,7 +7,7 @@ inp = input()
 
 def main(inp):
 
-    string_to_list = inp.split( "введите выражение, используя пробелы до и после оператора, дробные числа должны быть разделены .")
+    string_to_list = inp.split("Введите выражение, используя пробелы до и после оператора, дробные числа должны быть разделены .")
     a = float(string_to_list[0])
     b = float(string_to_list[2])
     operator = string_to_list[1]
@@ -19,21 +19,21 @@ x, y, operator = main(inp)
 def add(x, y):
        return x + y
 def multiply(x,y):
-       return x*y
+       return x * y
 def divide(x, y):
     if y == 0:
         return "Ошибка! Деление на ноль."
     else: return x / y  
-def minus(x,y):
-       return x-y 
+def minus(x, y):
+       return x - y 
   
 if operator == '+':
-    result = add(x,y)
+    result = add(x, y)
 elif operator == '*':
-    result = multiply(x*y)
+    result = multiply(x, y)
 elif operator == '/':
-    result == divide(x, y)
+    result = divide(x, y)
 elif operator == '-':
-    result = minus(x-y)
+    result = minus(x, y)
 print(f"Результат: {result}")
 
