fc5bad9cee321c25d7372754088c3ea8b94c2794
Polina <polina@LAPTOP-31KT7R30>
2024-09-15 18:08:18 +0700

    Initiate

--- calculator.py
+++ calculator.py
@@ -2,3 +2,4 @@
 # Input - user enters numbers and operation sign
 # Output - calculated number
 # Authors: Калинина, Козлова, Субракова, Финк
+expression = input("введите выражение, используя пробелы до и после оператора")
