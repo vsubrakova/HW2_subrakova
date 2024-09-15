8e8553f53018843183807a392d2d9c4ce5ab77df
Polina <polina@LAPTOP-31KT7R30>
2024-09-15 18:10:20 +0700

    Add variables for numbers, operator

--- calculator.py
+++ calculator.py
@@ -3,3 +3,7 @@
 # Output - calculated number
 # Authors: Калинина, Козлова, Субракова, Финк
 expression = input("введите выражение, используя пробелы до и после оператора")
+expression_split = expression.split()
+num1 = float(expression_split[0])  # первое число выражения
+operator = expression_split[1]  # оператор выражения
+num2 = float(expression_split[2])  # второе число выражения
