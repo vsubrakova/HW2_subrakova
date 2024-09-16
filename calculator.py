41933dc915e1f8215f73c3869a84ac2f2768611c
viburnum <viburnum@MacBook-Pro-Anna.local>
2024-09-16 18:43:32 +0300

    restructured main function

--- calculator.py
+++ calculator.py
@@ -3,22 +3,32 @@
 # Output - calculated number
 # Authors: Калинина, Козлова, Субракова, Финк
 
+def add(x, y):
+       return x + y
+def multiply(x,y):
+       return x * y
+def divide(x, y):
+    if y == 0:
+        return "Ошибка! Деление на ноль."
+    else: 
+        return x / y  
+def minus(x, y):
+       return x - y 
 
-inp = input()
-
-def main(inp):
-    string_to_list = inp.split(" ")
+def main():
+    inp = input("Введите выражение, используя пробелы до и после оператора, дробные числа должны быть разделены .")
+    string_to_list = inp.split( )
     a = float(string_to_list[0])
     b = float(string_to_list[2])
     operator = string_to_list[1]
-    return a, b, operator
-
-x, y, operator = main(inp)
-
-def add(x, y):
-       return x + y
-
-if operator == '+':
-    print(add(x,y))
-
-
+    
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '*':
+        result = multiply(a, b)
+    elif operator == '/':
+        result = divide(a, b)
+    elif operator == '-':
+        result = minus(a, b)
+    return result
+print(main())
