d99f2e76ada2ae03f16ff57e91bedaf46c1eac8b
vsubrakova <101574741+vsubrakova@users.noreply.github.com>
2024-09-20 00:09:16 +0200

    Update calculator.py - bug fix

--- HW2_subrakova/calculator.py
+++ HW2_subrakova/calculator.py
@@ -4,11 +4,11 @@
 # Authors: Калинина, Козлова, Субракова, Финк
 
 
-inp = input()
+inp = input("введите выражение, используя пробелы до и после оператора, дробные числа должны быть разделены .")
 
 def main(inp):
 
-    string_to_list = inp.split("введите выражение, используя пробелы до и после оператора, дробные числа должны быть разделены .")
+    string_to_list = inp.split( )
     a = float(string_to_list[0])
     b = float(string_to_list[2])
     operator = string_to_list[1]
@@ -22,11 +22,8 @@ def add(x, y):
 def multiply(x, y):
     return x * y
 def divide(x, y):
-    while y == 0:
-        print("Ошибка! Деление на ноль. Пожалуйста введите не нулевое значение для y")
-        y = float(input("Введите значение для y: "))
     return x / y
- def minus(x, y):
+def minus(x, y):
     return x - y 
   
 if operator == '+':
@@ -34,7 +31,7 @@ if operator == '+':
 elif operator == '*':
     result = multiply(x, y)
 elif operator == '/':
-    result == divide(x, y)
+    result = divide(x, y)
 elif operator == '-':
     result = minus(x, y)
 print(f"Результат: {result}")
