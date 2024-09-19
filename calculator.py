97848387bee97e9c8a0679b74653aeb4318fb4d7
vsubrakova <subrakova.vera@gmail.com>
2024-09-19 21:09:10 +0200

    Revert to the state of the project at 01d5467

--- calculator.py
+++
@@ -1,4 +0,0 @@
-# Description - program calculates basic mathematical operations: +,-,*,/
-# Input - user enters numbers and operation sign
-# Output - calculated number
-# Authors: Калинина, Козлова, Субракова, Финк
