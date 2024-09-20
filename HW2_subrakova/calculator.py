6e4a4113a43e72c3f3396fee87469ae164110b71
The-AGT <maksim.fink@pharminnotech.com>
2024-09-20 23:48:48 +0300

    Дополнение функции divid
    Теперь появляется уведомление при делении на ноль.

--- HW2_subrakova/calculator.py
+++ HW2_subrakova/calculator.py
@@ -21,7 +21,7 @@ def add(x, y):
 def multiply(x, y):
     return x * y
 def divide(x, y):
-    return x / y
+    return x / y if y != 0 else "Ошибка! Деление на ноль"
 def minus(x, y):
     return x - y 
   
